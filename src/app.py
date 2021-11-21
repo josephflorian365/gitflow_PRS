abeeae975829f611a7190a2da45b16c23835bed5
josephflorian365 <jflorianf@vallegrande.edu.pe>
2021-11-21 13:51:10 -0500

    feature: funcionalidad multiplicacion comentada

--- src/app.py
+++ src/app.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 def index():
     return "Valle Grande!"
 
+#Suma
 @app.route('/sum/<int:a>/<int:b>')
 def sum(a: int, b: int):
     nums_sum = a + b
     return f"La suma es: {str(nums_sum)}"
+
+#Multiplicacion
+# @app.route('/multiply/<int:a>/<int:b>')
+# def sum(a: int, b: int):
+#     nums_mul = a * b
+#     return f"La suma es: {str(nums_mul)}"
